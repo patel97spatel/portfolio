347dc15aebcc5f58c834b4c57c34e3ddc018bf7f
patel97spatel <patel97spatel@gmail.com>
2024-01-21 00:41:52 +0530

    Open active page setup and apply page animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,36 @@
 /* ======================== Typed Animation ======================= */
 
 var typed = new Typed(".typing", {
-    strings:["Web Designer","Web Developer","Graphic Designer","You Tuber"],
-    typeSpeed:100,
-    BackSpeed:60,
-    loop:true
+    strings: ["Web Designer", "Web Developer", "Graphic Designer", "You Tuber"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
 })
 
 /* ======================== Aside ======================= */
 
 const nav = document.querySelector(".nav"),
-navlist = nav.querySelectorAll("li"),
-totalNavlist = navlist.length;
-for(let i=0; i<totalNavlist; i++){
+    navlist = nav.querySelectorAll("li"),
+    totalNavlist = navlist.length,
+    allSection = document.querySelectorAll(".section"),
+    totalSection = allSection.length;
+for (let i = 0; i < totalNavlist; i++) {
     // console.log(navlist[i])
     const a = navlist[i].querySelector("a");
     // console.log(a)
-    a.addEventListener("click", function() {
-        for(let j=0; j<totalNavlist; j++){
+    a.addEventListener("click", function () {
+
+        for (let i = 0; i < totalSection; i++) {
+            allSection[i].classList.remove("back-section");
+        }
+        for (let j = 0; j < totalNavlist; j++) {
+
+            if (navlist[j].querySelector("a").classList.contains("active")) {
+
+                // console.log("back-section" + navlist[j].querySelector("a"))
+                allSection[j].classList.add("back-section");
+            }
+
             navlist[j].querySelector("a").classList.remove("active");
         }
         // console.log(this)
@@ -25,10 +38,14 @@ for(let i=0; i<totalNavlist; i++){
         showSection(this);
     })
 }
-function showSection(element){
+function showSection(element) {
+
+    for (let i = 0; i < totalSection; i++) {
+        allSection[i].classList.remove("active");
+    }
     // console.log(element.getAttribute("href").split("#"))
-    const href = element.getAttribute("href").split("#");
-    target = href[1];
-    console.log(target)
+    const target = element.getAttribute("href").split("#")[1];
+    //  console.log(target)
+    document.querySelector("#" + target).classList.add("active")
 
 }
