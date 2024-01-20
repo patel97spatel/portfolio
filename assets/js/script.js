99c45a6c565b2cc90a1e0f7d946d74a42773fb91
patel97spatel <patel97spatel@gmail.com>
2024-01-20 23:52:49 +0530

    manage navigation paging add js code

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,29 @@ var typed = new Typed(".typing", {
     BackSpeed:60,
     loop:true
 })
+
+/* ======================== Aside ======================= */
+
+const nav = document.querySelector(".nav"),
+navlist = nav.querySelectorAll("li"),
+totalNavlist = navlist.length;
+for(let i=0; i<totalNavlist; i++){
+    // console.log(navlist[i])
+    const a = navlist[i].querySelector("a");
+    // console.log(a)
+    a.addEventListener("click", function() {
+        for(let j=0; j<totalNavlist; j++){
+            navlist[j].querySelector("a").classList.remove("active");
+        }
+        // console.log(this)
+        this.classList.add("active")
+        showSection(this);
+    })
+}
+function showSection(element){
+    // console.log(element.getAttribute("href").split("#"))
+    const href = element.getAttribute("href").split("#");
+    target = href[1];
+    console.log(target)
+
+}
