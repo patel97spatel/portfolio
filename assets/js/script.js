79dce2215df07ed9a9c257879ef2a1196f608188
patel97spatel <patel97spatel@gmail.com>
2024-01-20 21:43:45 +0530

    add text typing  js query & Complete the Project

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+/* ======================== Typed Animation ======================= */
+
+var typed = new Typed(".typing", {
+    strings:["Web Designer","Web Developer","Graphic Designer","You Tuber"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
