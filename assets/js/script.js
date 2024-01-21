9795794432e3a53fe776e02b521964c2bf737432
patel97spatel <patel97spatel@gmail.com>
2024-01-21 11:13:18 +0530

    ad js code for active page open Management

--- assets/js/script.js
+++ assets/js/script.js
@@ -52,11 +52,16 @@ function showSection(element) {
 /*  ===================  Nav Toggler Button =========== */
 
 const navTogglerBtn = document.querySelector(".nav-toggler"),
-aside = document.querySelector(".aside");
-navTogglerBtn.addEventListener("click", function() {
+    aside = document.querySelector(".aside");
+navTogglerBtn.addEventListener("click", function () {
     asideSectionTogglerBtn();
 })
 
-function asideSectionTogglerBtn(){
+function asideSectionTogglerBtn() {
     aside.classList.toggle("open");
+    navTogglerBtn.classList.toggle("open");
+    
+    for (let i = 0; i < totalSection; i++) {
+        allSection[i].classList.toggle("open");
+    }
 }
