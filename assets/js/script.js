6e11c3ad3de83a075a5d5f91a9186bde38c4afbc
patel97spatel <patel97spatel@gmail.com>
2025-07-17 14:30:34 +0530

    v2 add some details

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 /* ======================== Typed Animation ======================= */
 
 var typed = new Typed(".typing", {
-    strings: ["Web Designer", "Web Developer", "Graphic Designer", "You Tuber"],
+    strings: ["Web Designer", "PHP Laravel Developer", "Graphic Designer"],
     typeSpeed: 100,
     BackSpeed: 60,
     loop: true
